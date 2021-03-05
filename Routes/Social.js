543721b3f4e7520a32c150de8d13364552320559
nav-commits <53131408+nav-commits@users.noreply.github.com>
2021-03-04 20:29:54 -0500

    created all apis for the app

--- Routes/Social.js
+++ Routes/Social.js
@@ -31,6 +31,58 @@ router.post('/',(req,res)=>{
  })
 
 
+// socialposts by id
+router.get('/:id', async(req,res)=>{
+    try {
+        const socials = await Social.findById(req.params.id);
+        res.json(socials);
+    }
+    catch(err){
+     res.status(500).json({message:err});
+    }
+});
+
+
+// delete alll socialposts
+router.delete('/', async(req,res)=>{
+    try {
+        const deletesocialposts = await Social.remove();
+        res.json(deletesocialposts);
+    }
+    catch(err){
+     res.status(500).json({message:err});
+    }
+})
+
+
+// delete socialposts by id
+router.delete('/:id', async(req,res)=>{
+    try {
+        const removesocialPost = await Social.remove({_id:req.params.id});
+        res.json(removesocialPost);
+    }
+    catch(err){
+     res.status(500).json({message:err});
+    }
+})
+
+
+// updated post by id
+router.patch('/:id',async(req,res)=>{
+    try {
+        const updatesocialPost = 
+        await Social.updateOne({_id:req.params.id}, 
+        {$set:{title: req.body.title, 
+        message: req.body.message ,
+        creater:req.body.creater, 
+        selectedFile:req.body.selectedFile }});
+        res.json(updatesocialPost);
+    }
+    catch(err){
+     res.status(500).json({message:err});
+    }
+})
+
 
 
 
