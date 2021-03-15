24eaed8a88f5a3592102fa017ae99aed37039e84
nav-commits <53131408+nav-commits@users.noreply.github.com>
2021-03-15 19:16:07 -0400

    fixed up the form and image as well as the cards

--- client/src/Components/Post.jsx
+++ client/src/Components/Post.jsx
@@ -57,6 +57,7 @@ axios.post('/Social',form)
 <div>
     <div className="Back-Img">
       <form className="Form" onSubmit={HandleSubmit}>
+        <h1 className="Soc-Post">Social Post</h1>
            <input type="text" placeholder="Title" value={form.title} onChange={e => setForm({...form,title:e.target.value})}/>
            <input type="text" placeholder="Message" value={form.message} onChange={e => setForm({...form,message: e.target.value})}/>
            <input type="text" placeholder="Creater" value={form.creater} onChange={e => setForm({...form,creater:e.target.value})}/>
