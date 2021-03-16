30eeb901f40eb712022d3901dd5816c9b23e6988
nav-commits <53131408+nav-commits@users.noreply.github.com>
2021-03-16 18:43:03 -0400

    created footer comp

--- client/src/Components/App.js
+++ client/src/Components/App.js
@@ -2,13 +2,14 @@ import { useContext } from "react";
 import Header from "../Components/Header";
 import "../Styles/App.css";
 import Post from "../Components/Post.jsx"
-
+import Footer from "../Components/Footer.jsx"
 
 function App() {
   return (
     <div>
        <Header/>
        <Post/>
+       <Footer/>
     </div>
   );
 }
---
+++ client/src/Components/Footer.jsx
@@ -0,0 +1,16 @@
+import "../Styles/App.css";
+
+
+
+function Footer() {
+  return (
+  <div>
+        <div className="Footer">
+           <h2 className="Footer-content">TheSocialPosts 365</h2>
+        </div>
+        
+</div>
+  );
+}
+
+export default Footer;
