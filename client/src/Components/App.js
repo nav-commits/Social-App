5071fd58211681248640547d564416361c502ba5
nav-commits <53131408+nav-commits@users.noreply.github.com>
2021-03-18 18:38:19 -0400

    refactored code for routes

--- client/src/Components/App.js
+++ client/src/Components/App.js
@@ -2,24 +2,22 @@ import Header from "../Components/Header";
 import "../Styles/App.css";
 import Post from "../Components/Post.jsx"
 import Footer from "../Components/Footer.jsx"
-import { BrowserRouter as Router, Route,Switch } from 'react-router-dom';
+import  {BrowserRouter as Router, Route ,Switch } from 'react-router-dom';
 import Home from '../Components/Home';
 
 
 function App() {
-  return (
-    
+  return ( 
   <Router>
         <div>
          <Header/>
          <Switch>
-             <Route path="/" exact component={Home} />
-             <Route path="/Posts" component={Post} />
+             <Route path="/" exact component={Home}/>
+             <Route path="/post" component={Post} /> 
          </Switch>
          <Footer/>
       </div>
   </Router>
-  
    
   );
 }
