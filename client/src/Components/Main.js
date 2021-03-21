f4023d15aee308eecc3852d6b3300c55fd4a7b13
nav-commits <53131408+nav-commits@users.noreply.github.com>
2021-03-21 14:30:36 -0400

    fixed up home page title

--- client/src/Components/Main.js
+++ client/src/Components/Main.js
@@ -3,8 +3,7 @@ import "../Styles/App.css";
 function Main() {
   return (
    <div>
-       <h1>Welcome to Posts</h1>
-       <p>There so many posts you will able post and read on the content</p>
+       <h1 className="Main-Posts">Welcome to Posts</h1>
    </div>
   );
 }
