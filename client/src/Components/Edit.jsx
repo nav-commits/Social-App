fdfbea2b261b4039704a0c88ca9136e1100d2b8f
nav-commits <53131408+nav-commits@users.noreply.github.com>
2021-03-21 22:37:21 -0400

    added update functionality

--- client/src/Components/Edit.jsx
+++ client/src/Components/Edit.jsx
@@ -9,7 +9,6 @@ import {useLocation} from 'react-router-dom';
 function Edit() {
 const history = useHistory();
 const location = useLocation();
-console.log(location.state.id);
 
 const Handleclick = () => {
     history.push('/post');
@@ -18,11 +17,10 @@ const {form,setForm} = useContext(PostContext);
 
 const HandleSubmit = (event) =>{
     event.preventDefault();
-    axios.update(`/Social/${location.state.id}`,form)
+    axios.patch(`/Social/${location.state.id}`,form)
       .then((response)=>{
       console.log(response)
       console.log(form);
-      
       setForm('');
       })
       .catch((error)=>{
@@ -32,10 +30,10 @@ const HandleSubmit = (event) =>{
     }
 
   return (
-   <div>
+<div>
     <button className="Go-Back" onClick={Handleclick}>Go Back</button>
-      <form className="Edit-Form" onSubmit={HandleSubmit}>
-        <h1 className="Soc-Post">Social Post</h1>
+        <form className="Edit-Form" onSubmit={HandleSubmit}>
+            <h1 className="Soc-Post">Social Post</h1>
            <input type="text" placeholder="Title" value={form.title} onChange={e => setForm({...form,title:e.target.value})}/>
            <input type="text" placeholder="Message" value={form.message} onChange={e => setForm({...form,message: e.target.value})}/>
            <input type="text" placeholder="Creater" value={form.creater} onChange={e => setForm({...form,creater:e.target.value})}/>
@@ -43,8 +41,8 @@ const HandleSubmit = (event) =>{
                <FileBase64 multiple={false} type="file" onDone={({base64}) => setForm({...form, selectedFile : base64})} />
            </div>
            <button className="Edit-Save">Save</button>
-  </form>
-   </div>
+      </form>
+</div>
   );
 }
 
